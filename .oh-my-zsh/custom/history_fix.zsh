68bf0c096da0d7c72936e12c7b32a13af6cb31ad
DannyAAM <code@saru.moe>
2015-09-26 22:11:09 +0800

    change history behavior to search with beginning

---
+++ .oh-my-zsh/custom/history_fix.zsh
@@ -0,0 +1,9 @@
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+
+bindkey '\eOA' up-line-or-beginning-search
+bindkey '\e[A' up-line-or-beginning-search
+bindkey '\eOB' down-line-or-beginning-search
+bindkey '\e[B' down-line-or-beginning-search
