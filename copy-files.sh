a60af67865008f8bc1fcecce55a18bfe24273955
DannyAAM <danny@saru.moe>
2024-10-05 05:30:24 +0800

    fix syntax for sh

--- copy-files.sh
+++ copy-files.sh
@@ -2,7 +2,7 @@
 cd $( dirname -- "$0"; )
 list=".zshrc .oh-my-zsh"
 for i in $list; do
-    if [[ -d $i ]]; then
+    if [ -d $i ]; then
         rsync -auK $i/. "$(readlink -f ~/$i)"
     else
         rsync -auK $i "$(readlink -f ~/$i)"
